21695758e867b22c3057d9e277c7990a69fd7b46
cqt <1273258724@qq.com>
2019-05-23 10:56:11 +0800

    Initial commit

---
+++ app/src/main/java/com/example/myhttp/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.myhttp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
